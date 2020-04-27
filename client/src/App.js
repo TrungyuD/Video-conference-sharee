272e95fe1b98e7a06de885c34c1da184df696c16
itechadmin <duy.nguyen@itechcorp.com.vn>
2020-04-27 17:58:33 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import CreateRoom from "./routes/CreateRoom";
-import Room from "./routes/Room";
-
+const RTCMultiConnection = require("rtcmulticonnection");
 function App() {
+  let connection = new RTCMultiConnection();
+  console.log('connection', connection);
+  
+  connection.socketURL = '/';
+
+  connection.socketMessageEvent = 'video-conference-demo';
+
+  connection.session = {
+    audio:true,
+    video:true
+  }
+  connection.sdpConstraints.mandatory = {
+    offerToReceiveAudio:true
+  }
+  const openRoom = () => {
+    
+  }
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={CreateRoom} />
-        <Route path="/room/:roomID" component={Room} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <h1 className="text-center">Video conferencing using RTCMultiConnection</h1>
+      <div className="text-center">
+          <div>
+            <label><input type="checkbox" id="record-entire-conference"/> Record Entire Conference In The Browser?</label>
+          <span id="recording-status"></span>
+          <button id="btn-stop-recording" style={{display:"none"}}>Stop Recording</button>
+          <br></br>
+
+          <input type="text" id="room-id" value="abcdef" />
+          <button id="open-room" onClick={openRoom}>Open Room</button>
+          <button id="join-room">Join Room</button>
+          <button id="open-or-join-room">Auto Open Or Join Room</button>
+        </div>
+
+        <div id="videos-container" style={{margin: "20px 0"}}></div>
+
+        <div id="room-urls" ></div>
+      </div>
+      
+    </div>
   );
 }
 
